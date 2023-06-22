4bcd5819029b8f9a12fbf6d5d1aeaea4824ed51d
LucasGambim <lucas.gambim.guedes@gmail.com>
2023-06-22 00:00:56 -0300

    adições gerais

--- imagem.py
+++ imagem.py
@@ -10,8 +10,8 @@ class imagem:
     
     def __init__(self, imagem):
         self.imagem = imagem
-        self.tresh = 0
-        self.paper = 0
+        
+        
     
     #este método aqui vai processar previamente a imagem até binarizar ela para que 
     #seja possivel extrair a informação das bolhas de opação de resposta de cada pergunta 
@@ -21,6 +21,8 @@ class imagem:
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         blurred = cv2.GaussianBlur(gray, (5, 5), 0)
         edged = cv2.Canny(blurred, 75, 200)
+        kernel = np.ones((10,10),np.uint8)
+        edged = cv2.dilate(edged,kernel,iterations = 1)
 
         cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         cnts = imutils.grab_contours(cnts)
@@ -35,54 +37,73 @@ class imagem:
                 if len(approx == 4):
                     doCnt = approx
                     break
+        paper = four_point_transform(image, doCnt.reshape(4, 2))
         warped = four_point_transform(gray, doCnt.reshape(4, 2))
-        self.thresh = cv2.threshold(warped, 
-                0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-        self.paper = four_point_transform(image, doCnt.reshape(4, 2))
+
+        warped1 = warped[40:2270, 50:1250]
+        warped2 = warped[43:2270, 1450:2670]
+        warped1 = cv2.resize(warped1, dsize=(390, 756))
+        warped2 = cv2.resize(warped2, dsize=(390, 756))
+        imagens_cortadas = [warped1, warped2]
+
+        imagens_pre_processadas = []
+        for imagem in imagens_cortadas:
+            thresh = cv2.threshold(imagem, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+            kernel = np.ones((4,4),np.uint8)
+            thresh = cv2.dilate(thresh,kernel,iterations = 1)
+            imagens_pre_processadas.append(thresh)
+
+
+        paper = four_point_transform(image, doCnt.reshape(4, 2))
+        paper2 = paper[43:2270, 1450:2670]
+        paper1 = paper[40:2270, 50:1250]
+        paper2 = cv2.resize(paper2, dsize=(379, 756))
+        paper1 = cv2.resize(paper1, dsize=(390, 756))
+        papers = [paper1, paper2]
+
+        return imagens_pre_processadas, papers
         
     
-    def processa_imagem(self):
-        ANSWER_KEY = {0: 1, 1: 0, 2: 0, 3: 3, 4: 1, 5: 3, 6: 3, 7: 2, 8: 2, 9: 2}
+    def processa_imagem(self, ANSWER_KEY, processadas, papers):
+       
+        for l in range(len(processadas)):
+            cnts = cv2.findContours(processadas[l].copy(), cv2.RETR_EXTERNAL, 
+            cv2.CHAIN_APPROX_SIMPLE)
+            ctns = imutils.grab_contours(cnts)
+            questionsCnts = []
 
-        cnts = cv2.findContours(self.thresh.copy(), cv2.RETR_EXTERNAL, 
-        cv2.CHAIN_APPROX_SIMPLE)
-        ctns = imutils.grab_contours(cnts)
-        questionsCnts = []
-
-        for c in ctns:  
-            x, y, w, h = cv2.boundingRect(c)
-            ar = w / float(h)
-            if w >= 20 and h >= 20 and ar >= 0.9 and ar <= 1.1:
-                questionsCnts.append(c)
+            for c in ctns:  
+                x, y, w, h = cv2.boundingRect(c)
+                ar = w / float(h)
+                if w >= 35 and h >= 35 and ar >= 0.9 and ar <= 1.1:
+                    questionsCnts.append(c)
         
-        questionCnts = contours.sort_contours(questionsCnts, method = "top-to-bottom")[0]
-        correct = 0 
-        for (q, i) in enumerate(np.arange(0, len(questionCnts), 5)):
-            cnts = contours.sort_contours(questionCnts[i:i + 5])[0]
-            bubbled = None
+            questionCnts = contours.sort_contours(questionsCnts, method = "top-to-bottom")[0]
+            correct = 0 
+            for (q, i) in enumerate(np.arange(0, len(questionCnts), 5)):
+                cnts = contours.sort_contours(questionCnts[i:i + 5])[0]
+                bubbled = None
 
-            for (j, c) in enumerate(cnts):
-                mask = np.zeros(self.thresh.shape, dtype="uint8")
-                cv2.drawContours(mask, [c], -1, 255, -1)
-                mask = cv2.bitwise_and(self.thresh, self.thresh, mask=mask)
-                total = cv2.countNonZero(mask)
+                for (j, c) in enumerate(cnts):
+                    mask = np.zeros(processadas[l].shape, dtype="uint8")
+                    cv2.drawContours(mask, [c], -1, 255, -1)
+                    mask = cv2.bitwise_and(processadas[l], processadas[l], mask=mask)
+                    total = cv2.countNonZero(mask)
 
 
-            if bubbled is None or total > bubbled[0]:
-                bubbled = (total, j)
+                    if bubbled is None or total > bubbled[0]:
+                        bubbled = (total, j)
     
-            cor = (0, 0, 255)
-            k = ANSWER_KEY[q]
+                cor = (0, 0, 255)
+                k = ANSWER_KEY[l][q]
 
-            if k == bubbled[1]:
-                cor = (0, 255, 0)
-                correct += 1
+                if k == bubbled[1]:
+                    cor = (0, 255, 0)
+                    correct += 1
 
-            cv2.drawContours(self.paper, [cnts[k]], -1, cor, 2)
-        cv2.imshow('',self.paper)
-        cv2.waitKey(0)
+                cv2.drawContours(papers[l], [cnts[k]], -1, cor, 2)
 
-        return correct
+        return correct, papers
         
     def set_imagem(self, img):
         self.imagem = img
--- main.py
+++ main.py
@@ -3,12 +3,18 @@ import cv2
 
 def main():
     
+    ANSWER_KEY = [{0: 1, 1: 0, 2: 0, 3: 3, 4: 1, 5: 3, 6: 3, 7: 2, 8: 2, 9: 2}, 
+                  {0: 1, 1: 0, 2: 0, 3: 3, 4: 1, 5: 3, 6: 3, 7: 2, 8: 2, 9: 2}]
 
-    img = cv2.imread('Respostas.png')
+
+
+    img = cv2.imread('IMG_6841 (1).JPG')
     image = imagem(img)
-    image.pre_processa_imagem()
-    image.processa_imagem()
-
+    processadas, papers = image.pre_processa_imagem()
+    pontuação, papers = image.processa_imagem(ANSWER_KEY, processadas, papers)
+    imagem_pronta = cv2.hconcat(papers)
+    cv2.imshow('', imagem_pronta)
+    cv2.waitKey(0)
 
 if(__name__ == "__main__"):
     main()

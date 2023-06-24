8010d2570975a2e570ee976010c221f832bed978
LucasGambim <lucas.gambim.guedes@gmail.com>
2023-06-24 19:13:15 -0300

    adicionado o inicio do html

--- main.py
+++ main.py
@@ -1,33 +1,40 @@
 from imagem import imagem
 import cv2
-import streamlit as st
-from pages import page_criar_prova, page_corrigir_provas, page_processar_dados
+from flask import Flask, render_template, redirect, request
+import numpy as np
 
 
-def main():
-    with open("style/style.css") as f:
-        st.markdown(f"<style>{f.read()}<style>", unsafe_allow_html= True)
+app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-     
-    menu, opcoes = st.columns([2, 5])
+@app.route('/corretor', methods = ['POST', ])
+def corretor():
+
+    ANSWER_KEY = [{0: 0, 1: 0, 2: 0, 3: 3, 4: 1, 5: 3, 6: 3, 7: 2, 8: 2, 9: 2}, 
+                  {0: 1, 1: 0, 2: 0, 3: 3, 4: 1, 5: 3, 6: 3, 7: 2, 8: 2, 9: 2}]
     
-    with menu:
-        menu_select = st.selectbox('selecione a página', ('Criar Prova', 'Corrigir Prova', 'Dados'))
+    img = cv2.imread('IMG_6841 (1).JPG')
+    image = imagem(img)
+    processadas, papers = image.pre_processa_imagem()
+    pontuação, papers = image.processa_imagem(ANSWER_KEY, processadas, papers)
+    imagem_pronta = cv2.hconcat(papers)
+    cv2.imshow('', imagem_pronta)
+    cv2.waitKey(0)
 
-    with opcoes:
-        st.title('Corretor de provas')
+    return redirect('/')
 
-        if menu_select == 'Criar Prova':
-            page_criar_prova()
 
-        elif menu_select == 'Corrigir Prova':
-            page_corrigir_provas()
+@app.route('/corrigirprovas')
+def corrigir():
+    return render_template('corrigirprovas.html')
 
-        elif menu_select == 'Dados':
-            page_processar_dados()
-        
-    
+@app.route('/criarprovas')
+def criar_provas():
+    return render_template('criarprovas.html')
 
-if(__name__ == "__main__"):
-    main()
+
+if __name__ == "__main__":
+    app.run(debug=True)

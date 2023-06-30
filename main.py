b4f9dd1c6338fda111dc79a2ce68de6a61991940
GambimL <lucas.gambim.guedes@gmail.com>
2023-06-30 11:58:42 -0300

    adições gerais

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from imagem import imagem
 import cv2
+from tkinter import *
+from tkinter import ttk
 
 def main():
     
 
-    img = cv2.imread('Respostas.png')
+    ANSWER_KEY = [{0: 0, 1: 0, 2: 0, 3: 3, 4: 1, 5: 3, 6: 3, 7: 2, 8: 2, 9: 2}, 
+                  {0: 1, 1: 0, 2: 0, 3: 3, 4: 1, 5: 3, 6: 3, 7: 2, 8: 2, 9: 2}]
+    
+    img = cv2.imread('IMG_6841 (1).JPG')
     image = imagem(img)
-    image.pre_processa_imagem()
-    image.processa_imagem()
+    processadas, papers = image.pre_processa_imagem()
+    pontuação, papers = image.processa_imagem(ANSWER_KEY, processadas, papers)
+    imagem_pronta = cv2.hconcat(papers)
+    cv2.imshow('', imagem_pronta)
+    cv2.waitKey(0)
 
 
 if(__name__ == "__main__"):

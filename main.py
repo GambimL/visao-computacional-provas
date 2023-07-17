af711571543349124021c6efb5b2227b3eed2c38
LucasGambim <lucas.gambim.guedes@gmail.com>
2023-07-17 19:39:54 -0300

    commit de testes

--- main.py
+++ main.py
@@ -2,19 +2,35 @@ from imagem import imagem
 import cv2
 from tkinter import *
 from tkinter import ttk
+from PIL import Image, ImageTk
 
 def main():
-    
     webcam = cv2.VideoCapture(0)
-
+    janela = Tk()
     def generate_frames():
         while True:
             validation, frame = webcam.read()
             if not validation:
                 break
             else:
-                ret, buffer =  cv2.imencode('.jpg', frame)
-                frame = buffer.tobytes()
+                cv2.imshow("Video da Webcam", frame)
+                cv2.waitKey(5)
+
+    label_widget = Label(janela, generate_frames)
+    label_widget.pack()
+    button1 = Button(janela, text="Open Camera", command=generate_frames)
+    button1.pack()      
+
+    janela.title('corretor de provas')
+
+    janela.mainloop()
+    
+    
+    webcam = cv2.VideoCapture(0)
+
+
+    
+    label_widget = Label(janela, )
 
 
     ANSWER_KEY = [{0: 0, 1: 0, 2: 0, 3: 3, 4: 1, 5: 3, 6: 3, 7: 2, 8: 2, 9: 2}, 
